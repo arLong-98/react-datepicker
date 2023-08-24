142172f4a080abd8c96aeb950039c3cb2228b531
arLong-98 <dhairya999pateriya@gmail.com>
2023-08-25 03:18:58 +0530

    fix: css dev script

--- docs-site/src/components/App/index.js
+++ docs-site/src/components/App/index.js
@@ -31,6 +31,13 @@ const Example = () => {
         setIsOpen(false);
       }}
       onInputClick={() => setIsOpen(true)}
+      holidays={[
+        { date: "2023-01-26", holidayName: "Republic Day" },
+        { date: "2023-08-15", holidayName: "Independence Day" },
+        { date: "2023-12-25", holidayName: "Christmas" },
+        { date: "2023-11-12", holidayName: "Diwali" },
+        { date: "2023-10-02", holidayName: "Gandhi Jayanti" },
+      ]}
     />
   );
 };
@@ -56,61 +63,6 @@ const Root = () => (
         </div>
       </div>
     </div>
-    <div className="wrapper">
-      <h1>React Datepicker</h1>
-      <p className="badges">
-        <a href="https://npmjs.org/package/react-datepicker">
-          <img
-            src="https://badge.fury.io/js/react-datepicker.svg"
-            alt="NPM package version badge"
-            className="badge"
-          />
-        </a>
-        <a href="https://github.com/Hacker0x01/react-datepicker/actions/workflows/test.yml">
-          <img
-            src="https://github.com/Hacker0x01/react-datepicker/actions/workflows/test.yml/badge.svg"
-            alt="Test suite status badge"
-            className="badge"
-          />
-        </a>
-        <a href="https://david-dm.org/Hacker0x01/react-datepicker">
-          <img
-            src="https://david-dm.org/Hacker0x01/react-datepicker.svg"
-            alt="Dependency status badge"
-            className="badge"
-          />
-        </a>
-        <a href={"https://npmjs.org/package/react-datepicker"}>
-          <img
-            src="https://img.shields.io/npm/dm/react-datepicker.svg"
-            alt="Download count badge"
-            className="badge"
-          />
-        </a>
-      </p>
-      <p>A simple and reusable datepicker component for React.</p>
-
-      <h2>Installation</h2>
-      <p>The package can be installed via NPM:</p>
-      <p>
-        <code>npm install react-datepicker --save</code>
-      </p>
-      <p>Or by using Yarn:</p>
-      <p>
-        <code>yarn add react-datepicker</code>
-      </p>
-      <p>
-        Below are examples which also can be edited directly via the editor on
-        the left side and will be rendered on the right.
-      </p>
-    </div>
-    <div className="wrapper">
-      <ExampleComponents />
-    </div>
-
-    <a href="https://github.com/Hacker0x01/react-datepicker/">
-      <img className="github-ribbon" src={ribbon} alt="Fork me on GitHub" />
-    </a>
   </div>
 );
 
